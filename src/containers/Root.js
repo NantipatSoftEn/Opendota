9ce40cfbb87ccd029a504bdedc5f6d3677ec2824
Ｎａｎｔｉｐａｔ <keymasterviriya1150@gmail.com>
2017-12-22 15:16:40 +0700

    redux pattern :sparkles: :art:

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-import configureStore from './store/configureStore'
-import AppRouter from './routers/AppRouter';
+import configureStore from '../configureStore'
+import AsyncApp from './AsyncApp'
 
 const store = configureStore()
 
@@ -9,7 +9,7 @@ export default class Root extends Component {
   render() {
     return (
       <Provider store={store}>
-        <AppRouter />
+        <AsyncApp />
       </Provider>
     )
   }
---
+++ src/routers/AppRouter.js
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import home from '../containers/home';
+import AsyncApp from '../containers/AsyncApp';
+const AppRouter = () => (
+    <div>
+      <home />
+      <AsyncApp />
+    </div>
+);
+export default AppRouter;
